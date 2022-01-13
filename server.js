ba42979a9d645a1ae6d49826e69835001f8fe009
Christina Starr <christinastarr19@gmail.com>
2022-01-12 20:28:24 -0500

    Added broilerplate to server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.get("/api/config", (req, res) => {res.json({success:true,})
+});
+
+if (process.env.NODE_ENV === "production"){app.use(express.static("client/build"));}
+
+app.use(routes);
+
+app.get(*), (req, res) => {res.sendFile(path.join(__dirname, "./client/build/index.html"))};
+//Connect to the Mongo DB
